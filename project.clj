6e96b5a011f1674fdb918bbfa1608908ce62dd97
Dan Brook <broq@cpan.org>
2011-10-10 22:35:37 +0100

    Fill in remaining functionality now JSON API is in place.
    The plugin will now call the 4clojure JSON API, drop the replace-me stub test,
    add a problem stub test into the src and only add the problem test if there
    isn't one already. All this assumes that core.clj is the main src/test for the
    current project.

--- project.clj
+++ project.clj
@@ -19,30 +19,57 @@
 (defn title->fn [title]
   (.toLowerCase (cs/replace title #"[^\w]" "-")))
 
+(defn drop-replace-me [fn]
+  (let [t (slurp fn)
+        r #"(?m)^\(deftest replace-me[^)]+\)+$"]
+    (if (re-find r t)
+      (spit fn (cs/replace t r "")))))
+
+(defn add-stub [project prob]
+  (let [src (file "src" (ns->path (project :group)) "core.clj")]
+    (if-not (re-find (re-pattern (str "(?m)^\\(defn " prob)) (slurp src))
+      (spit src
+            (str "\n(defn " prob "-solution [] ; Update args as needed!\nnil\n)\n")
+            :append true))))
+
+(defn no-test-yet [fn prob]
+  (nil? (re-find (re-pattern (str "(?m)^\\(deftest can-" prob)) (slurp fn))))
+
+;; Seeing these four characters repeated was making my eyes bleed.
+(def Q "\"")
+;; Allow something that might have quotes to live in a string.
+(defn enquote [s]
+  (cs/replace s  Q "\\\""))
 ;; XXX - Format too?
 (defn expand-prob [prob tests]
   (->>
    (map #(cs/replace % #"\b__\b" (str prob "-solution")) tests)
-   (map #(cs/replace % "\\n" "\n"))
-   (clojure.string/join "\n")))
+   (map #(cs/replace % "\\r?\\n" "\n")) ; XXX \r isn't getting replaced.
+   (map #(cs/join " " ["(is" % Q (enquote %) Q ")"]))
+   (cs/join "\n")))
 
 (defn write-tests [project problem]
-  (let [prob (title->fn (problem :title))]
-    (spit (file "test" (ns->path (project :group)) "test" "core.clj")
-          (str "\n(deftest can-" prob "\n" (expand-prob prob (problem :tests)) "\n)\n")
-          :append true)))
+  (let [prob      (title->fn (problem :title))
+        test-file (file "test" (ns->path (project :group)) "test" "core.clj")]
+    (add-stub project prob)
+    (drop-replace-me test-file)    
+    (if (no-test-yet test-file prob)
+      (spit test-file
+            (str "\n(deftest can-" prob "\n" (expand-prob prob (problem :tests)) "\n)\n")
+            :append true)
+      true)))
 
-;; TODO - Pay attention to args once JSON API hits 4clojure.
-;;      - Write solution stub in src.
-;;      - Don't add deftest if one already exists.
-(defn foreclojure-problem [project & args]
+;; TODO - Turn into a proper plugin then upload it to clojars.
+(defn foreclojure-problem [project prob-num]
   (try
-    (let [json (read-json (string (http-agent "http://localhost/example.json" :method "GET")))]
-      (write-tests project json)
-      (println "Problem added!"))
+    ;; TODO - Check HTTP status.
+    (let [json (read-json (string (http-agent (str "http://4clojure.com/api/problem/" prob-num) :method "GET")))]
+      (if-not (write-tests project json) ; spit returns nil by default hence the inversion of logic.
+        (println (cs/join "" ["Problem " Q (json :title) Q " added!"]))
+        (println (cs/join "" ["Problem " Q (json :title) Q " has already been added!"]))
+      ))
     (catch Exception e
-      (println (str "Failed setting problem" args ": " (.getMessage e)))
+      (println (str "Failed setting problem " prob-num ": " (.getMessage e)))
       (shutdown-agents)
       )))
 
-; {:compile-path "/home/dbrook/dev/4clojure/foreclojure-plugin/classes", :group "foreclojure-plugin", :source-path "/home/dbrook/dev/4clojure/foreclojure-plugin/src", :dependencies [[org.clojure/clojure "1.2.1"] [org.clojure/clojure-contrib "1.2.0"]], :dev-dependencies nil, :name "foreclojure-plugin", :root "/home/dbrook/dev/4clojure/foreclojure-plugin", :jar-dir "/home/dbrook/dev/4clojure/foreclojure-plugin", :version "1.0.0-SNAPSHOT", :jar-exclusions [#"^\."], :test-path "/home/dbrook/dev/4clojure/foreclojure-plugin/test", :test-resources-path "/home/dbrook/dev/4clojure/foreclojure-plugin/test-resources", :target-dir "/home/dbrook/dev/4clojure/foreclojure-plugin", :uberjar-exclusions [#"^META-INF/DUMMY.SF"], :dev-resources-path "/home/dbrook/dev/4clojure/foreclojure-plugin/test-resources", :library-path "/home/dbrook/dev/4clojure/foreclojure-plugin/lib", :resources-path "/home/dbrook/dev/4clojure/foreclojure-plugin/resources", :native-path "/home/dbrook/dev/4clojure/foreclojure-plugin/native", :description "FIXME: write description"}
