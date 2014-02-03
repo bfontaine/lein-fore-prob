e15bede137fee23cd43e387f33b22fd278d7184b
Baptiste Fontaine <batifon@yahoo.fr>
2014-02-03 11:22:27 +0100

    upgrading to Leiningen 2.x

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject foreclojure-plugin "0.0.1"
   :description "A leiningen plugin to populate a project from a 4clojure problem."
   :url "https://github.com/broquaint/lein-foreclojure-plugin"
-  :dev-dependencies [[lein-clojars "0.6.0"]]
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[clj-http "0.7.8"]
+                 [cheshire "5.3.1"]]
+  :eval-in-leiningen true)
--- src/leiningen/fore_prob.clj
+++ src/leiningen/fore_prob.clj
@@ -1,70 +1,73 @@
 (ns leiningen.fore-prob
   "Populate the current project with a 4clojure problem."
-  (:use [clojure.contrib.http.agent :only(http-agent string)]
-        [clojure.contrib.json]
-        [clojure.java.io :only [file]]
-        [clojure.string :as cs :exclude [replace reverse]])
-  )
+  (:require [clj-http.client :as http]
+            [cheshire.core   :as json]
+            [clojure.java.io :as io])
+  (:use [clojure.string :as cs :exclude [replace reverse]]))
 
 ;; Purloined from leiningen/src/leiningen/util/paths.clj
-(defn ns->path [n]
+(defn- ns->path [n]
   (str (.. (str n)
            (replace \- \_)
            (replace \. \/))))
 
-(defn title->fn [title]
+(defn- title->fn [title]
   (.toLowerCase (cs/replace title #"[^\w]" "-")))
 
-(defn drop-replace-me [fn]
+(defn- drop-replace-me [fn]
   (let [t (slurp fn)
         r #"(?m)^\(deftest replace-me[^)]+\)+$"]
     (if (re-find r t)
       (spit fn (cs/replace t r "")))))
 
-(defn add-stub [project prob]
-  (let [src (file "src" (ns->path (project :group)) "core.clj")]
+(defn- add-stub [project prob]
+  (let [src (io/file "src" (ns->path (project :group)) "core.clj")]
     (if-not (re-find (re-pattern (str "(?m)^\\(defn " prob)) (slurp src))
       (spit src
-            (str "\n(defn " prob "-solution [] ; Update args as needed!\nnil\n)\n")
+            (str "\n(defn " prob "-solution [] ; Update args as needed!\n"
+                 "nil\n)\n")
             :append true))))
 
-(defn no-test-yet [fn prob]
+(defn- no-test-yet [fn prob]
   (nil? (re-find (re-pattern (str "(?m)^\\(deftest can-" prob)) (slurp fn))))
 
-;; Seeing these four characters repeated was making my eyes bleed.
-(def Q "\"")
 ;; Allow something that might have quotes to live in a string.
-(defn enquote [s]
-  (cs/replace s  Q "\\\""))
+(defn- enquote [s]
+  (cs/replace s  "\"" "\\\""))
 ;; XXX - Format too?
-(defn expand-prob [prob tests]
+(defn- expand-prob [prob tests]
   (->>
    (map #(cs/replace % #"\b__\b" (str prob "-solution")) tests)
    (map #(cs/replace % "\\r?\\n" "\n")) ; XXX \r isn't getting replaced.
-   (map #(cs/join " " ["(is" % Q (enquote %) Q ")"]))
+   (map #(cs/join " " ["(is" % "\"" (enquote %) "\")"]))
    (cs/join "\n")))
 
-(defn write-tests [project problem]
+(defn- write-tests [project problem]
   (let [prob      (title->fn (problem :title))
-        test-file (file "test" (ns->path (project :group)) "test" "core.clj")]
+        test-file (io/file
+                    "test" (ns->path (project :group)) "test" "core.clj")]
     (add-stub project prob)
     (drop-replace-me test-file)    
     (if (no-test-yet test-file prob)
       (spit test-file
-            (str "\n(deftest can-" prob "\n" (expand-prob prob (problem :tests)) "\n)\n")
+            (str "\n(deftest can-" prob "\n"
+                 (expand-prob prob (problem :tests)) "\n)\n")
             :append true)
       true)))
+
+(def fore-url "http://4clojure.com/api/problem/")
 
 ;; TODO - Create projects from a problem (maybe use the spawn plugin?).
 (defn fore-prob [project prob-num]
   (try
     ;; TODO - Check HTTP status.
-    (let [json (read-json (string (http-agent (str "http://4clojure.com/api/problem/" prob-num) :method "GET")))]
-      (if-not (write-tests project json) ; spit returns nil by default hence the inversion of logic.
-        (println (cs/join "" ["Problem " Q (json :title) Q " added!"]))
-        (println (cs/join "" ["Problem " Q (json :title) Q " has already been added!"]))
-      ))
+    (let [json (json/parse-string
+                 (:body (http/get (str fore-url prob-num))))
+          title (json :title)]
+      (if-not (write-tests project json) ; spit returns nil by default hence
+                                         ; the inversion of logic.
+        (println (str "Problem \"" title "\" added!"))
+        (println (str "Problem \"" title "\" has already been added!"))))
     (catch Exception e
       (println (str "Failed setting problem " prob-num ": " (.getMessage e)))
-      (shutdown-agents)
-      )))
+      (shutdown-agents))))
