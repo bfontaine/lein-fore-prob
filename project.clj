fac412ee6cc5a1cda957dc19e9fae0f85ec208e0
Baptiste Fontaine <batifon@yahoo.fr>
2014-02-10 23:05:28 +0100

    cheshire & clj-http-fake are only used for tests

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[clj-http              "0.7.8"]
-                 [clj-http-fake         "0.7.8"]
-                 [cheshire              "5.3.1"]
                  [clj-soup/clojure-soup "0.1.1"]]
+  :profiles {:dev {:dependencies [[cheshire      "5.3.1"]
+                                  [clj-http-fake "0.7.8"]]}}
   :eval-in-leiningen true)
