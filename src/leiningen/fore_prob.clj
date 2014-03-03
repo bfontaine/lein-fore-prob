4dfe73eb6fddfd35137a53883d037092b45915c4
Baptiste Fontaine <batifon@yahoo.fr>
2014-03-03 08:39:02 +0100

    minor memory usage improvement

--- src/leiningen/fore_prob.clj
+++ src/leiningen/fore_prob.clj
@@ -191,5 +191,5 @@
 (defn fore-prob
   "main function, used by leiningen"
   [project & prob-nums]
-  (doall
-    (map (partial add-prob project) prob-nums)))
+  (doseq [n prob-nums]
+    (add-prob project n)))
