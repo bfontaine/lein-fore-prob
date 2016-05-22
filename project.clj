50cf3e6d068ad217e20b38abda592b1a5a215608
Baptiste Fontaine <b@ptistefontaine.fr>
2016-05-22 23:13:21 +0200

    Test with Clojure 1.7 & 1.8

--- project.clj
+++ project.clj
@@ -9,6 +9,8 @@
                                   [clj-http-fake "0.7.8"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-beta2"]]}}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}}
   :eval-in-leiningen true
   :plugins [[lein-cloverage "1.0.2"]])
