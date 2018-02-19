5e4a1b92dc47e43b3935c9106a3ad309231e5305
Baptiste Fontaine <b@ptistefontaine.fr>
2018-02-20 00:37:12 +0100

    bump deps
    Fixes #6.

--- project.clj
+++ project.clj
@@ -1,16 +1,18 @@
 (defproject lein-fore-prob "0.1.3-SNAPSHOT"
-  :description "A leiningen plugin to populate a project from a 4clojure problem."
+  :description "Leiningen plugin to populate a project from a 4clojure problem."
   :url "https://github.com/bfontaine/lein-fore-prob"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[clj-http              "0.9.0"]
-                 [clj-soup/clojure-soup "0.1.1"]]
-  :profiles {:dev {:dependencies [[cheshire      "5.3.1"]
-                                  [clj-http-fake "0.7.8"]]}
+  :dependencies [[clj-http              "3.7.0"]
+                 [clj-soup/clojure-soup "0.1.3"]]
+  :profiles {:dev {:dependencies [[cheshire      "5.8.0"]
+                                  [clj-http-fake "1.0.3"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
+             }
   :eval-in-leiningen true
   :plugins [[lein-cloverage "1.0.2"]])
