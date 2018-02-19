080b7d1d1acb73c200a2139be64cd2293dec1c20
Baptiste Fontaine <b@ptistefontaine.fr>
2018-02-20 00:51:41 +0100

    Add 1.3 support

--- project.clj
+++ project.clj
@@ -5,14 +5,16 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[clj-http              "3.7.0"]
                  [clj-soup/clojure-soup "0.1.3"]]
-  :profiles {:dev {:dependencies [[cheshire      "5.8.0"]
+  :profiles {:dev {:dependencies [[cheshire      "5.5.0"]
                                   [clj-http-fake "1.0.3"]]}
+
+             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
-             :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
-             }
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}}
+  :aliases {"all" ["with-profile" "dev,1.3:dev,1.4:dev,1.5:dev,1.7:dev,1.8:dev,1.9:dev"]}
   :eval-in-leiningen true
   :plugins [[lein-cloverage "1.0.2"]])
